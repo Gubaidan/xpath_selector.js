1a2b3ebe71b2f347be1f13738bf6cc0f55ef39f9
Mark Gandolfo <mark@aussiev8.com.au>
2014-01-17 17:49:50 +1000

    init2

---
+++ element_selector.js
@@ -0,0 +1,38 @@
+javascript:(function() {
+
+    var newcss = ".mouseOn{background-color: #bcd5eb !important;outline: 2px solid #5166bb !important;}";
+    if ('\v'=='v') /* ie only */ {
+        document.createStyleSheet().cssText = newcss;
+    } else {
+        var tag = document.createElement('style'); tag.type = 'text/css'; document.getElementsByTagName('head')[0].appendChild(tag); 
+        tag[ (typeof document.body.style.WebkitAppearance=='string') /* webkit only */ ? 'innerText' : 'innerHTML'] = newcss;    
+    }
+
+  prevElement = null;
+  document.addEventListener('mousemove',
+      function(e){
+          var elem = e.target || e.srcElement;
+          if (prevElement!= null) {prevElement.classList.remove("mouseOn");}
+          elem.classList.add("mouseOn");
+          prevElement = elem;
+      },true);
+  
+  document.addEventListener('click', 
+    function(e){
+      var pathElements = [];
+      var elem = e.currentTarget;
+      var index = 0;
+      var siblings = e.currentTarget.parentNode.getElementsByTagName(e.currentTarget.tagName);
+      for (var i=0, imax=siblings.length; i<imax; i++) {
+        if (e.currentTarget === siblings[i] {
+          index = i+1; // add 1 for xpath 1-based
+        }
+      }
+
+      while (elem.tagName.toLowerCase() != "html") {
+        pathElements.unshift(elem.tagName);
+        elem = elem.parentNode;
+      }
+      console.log(pathElements.join("/") + "[" + index + "]");
+    });
+})();
